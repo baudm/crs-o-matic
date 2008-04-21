49af8f6765f8beedbad13d960ad876b14f92f1d7
Darwin Bautista <daruuin@gmail.com>
2008-04-21 07:55:18 +0000

    Moved trunk/benchmarks to branches/tests.

--- benchmarks/testcomb.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from pure import combine
-
-ctr = 0
-for a in combine(range(25), range(20), range(30), range(50)): ctr += 1
-print ctr
--- benchmarks/testcombx.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from pure import combinex
-
-ctr = 0
-for a in combinex(range(25), range(20), range(30), range(50)): ctr += 1
-print ctr
