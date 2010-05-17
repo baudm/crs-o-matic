5bb954a3192227edbb5402765c985a98dc8c6e5d
Darwin Bautista <daruuin@gmail.com>
2010-05-17 07:25:21 +0000

    Added the pure Python implementation of itertools.product() as a last resort (copied from http://docs.python.org/library/itertools.html#itertools.product)

--- crs.py
+++ crs.py
@@ -31,9 +31,23 @@ except NameError:
 try:
     from itertools import product
 except ImportError:
-    from probstat import Cartesian
-    def product(*params):
-        return Cartesian(list(params))
+    try:
+        from probstat import Cartesian
+    except ImportError:
+        # Equivalent itertools.product() implementation copied from
+        # http://docs.python.org/library/itertools.html#itertools.product
+        def product(*args, **kwds):
+            # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+            # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+            pools = map(tuple, args) * kwds.get('repeat', 1)
+            result = [[]]
+            for pool in pools:
+                result = [x+[y] for x in result for y in pool]
+            for prod in result:
+                yield tuple(prod)
+    else:
+        def product(*args):
+            return Cartesian(list(args))
 
 
 AYSEM = '120093'
