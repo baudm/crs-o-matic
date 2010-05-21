37cde7faf199230e956c9c59afd3535bbb6a8488
Darwin Bautista <daruuin@gmail.com>
2010-05-21 11:15:24 +0000

    pagination.js: Use arrow images and fading effect for transition

--- js/pagination.js
+++ js/pagination.js
@@ -1,28 +1,26 @@
 
 function changePage(page) {
-	$('.current').hide().removeClass('current');
-	$('#p'+page).show().addClass('current');
+	$('.current').fadeOut().removeClass('current');
+	$('#p'+page).fadeIn().addClass('current');
 }
 
 
 $(document).ready(function () {
 	$('#p1').show().addClass('current');
-    var pages = $('.page').length;
-    if (pages > 1) {
-        $('.pagination').paginate({
-            count: pages,
-            start: 1,
-            display: 7,
-            border: true,
-            border_color: '#fff',
-            text_color: '#fff',
-            background_color: 'black',	
-            border_hover_color: '#ccc',
-            text_hover_color: '#000',
-            background_hover_color: '#fff', 
-            images: false,
-            mouse: 'press',
-            onChange: changePage
-        });
-    }
+	var pages = $('.page').length;
+	pages > 1 && $('.pagination').paginate({
+		count: pages,
+		start: 1,
+		display: 7,
+		border: true,
+		border_color: '#fff',
+		text_color: '#fff',
+		background_color: 'black',	
+		border_hover_color: '#ccc',
+		text_hover_color: '#000',
+		background_hover_color: '#fff',
+		images: true,
+		mouse: 'press',
+		onChange: changePage
+	});
 });
