1e087ff62b7d9c586ab94f9cf52a91d606c38fec
Darwin Bautista <daruuin@gmail.com>
2011-02-08 19:55:04 +0800

    Remove support for the probstat C extension module

--- crs.py
+++ crs.py
@@ -27,27 +27,20 @@ try:
     set
 except NameError:
     from sets import Set as set
-# Prefer itertools.product over probstat.Cartesian
 try:
     from itertools import product
 except ImportError:
-    try:
-        from probstat import Cartesian
-    except ImportError:
-        # Equivalent itertools.product() implementation copied from
-        # http://docs.python.org/library/itertools.html#itertools.product
-        def product(*args, **kwds):
-            # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
-            # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
-            pools = map(tuple, args) * kwds.get('repeat', 1)
-            result = [[]]
-            for pool in pools:
-                result = [x+[y] for x in result for y in pool]
-            for prod in result:
-                yield tuple(prod)
-    else:
-        def product(*args):
-            return Cartesian(list(args))
+    # Equivalent itertools.product() implementation copied from
+    # http://docs.python.org/library/itertools.html#itertools.product
+    def product(*args, **kwds):
+        # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+        # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+        pools = map(tuple, args) * kwds.get('repeat', 1)
+        result = [[]]
+        for pool in pools:
+            result = [x+[y] for x in result for y in pool]
+        for prod in result:
+            yield tuple(prod)
 
 
 AYSEM = '120102'
