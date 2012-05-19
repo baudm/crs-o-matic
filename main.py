7c1fa46818f2abd4579567769b797027189f19f2
Darwin Bautista <daruuin@gmail.com>
2012-05-19 12:23:30 +0800

    Use Python 2.7 runtime

--- main.py
+++ main.py
@@ -20,9 +20,6 @@ import cgi
 import os.path
 import operator
 
-from google.appengine.dist import use_library
-use_library('django', '1.2')
-
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
