1b30a05b0f5782a56e78f71281e395b3f8f5841e
Darwin Bautista <daruuin@gmail.com>
2019-07-22 21:39:03 +0800

    Update deployment code for gcloud SDK

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-[ $# -eq 1 ] || exit 1
-APPCFG="$1"
-[ -x "$APPCFG" ] || exit 1
+[ -x "$(which gcloud)" ] || exit 1
 
 # Timestamp
 sed "s|LAST_UPDATE_DATE|`date`|" -i'.orig' index.html
 # Deploy
-$APPCFG update .
+gcloud app deploy --project crs-o-matic-hrd
 # Restore
 mv index.html.orig index.html
